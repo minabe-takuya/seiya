b6442d2d94a2ef6a994ceb418bc60e31e995b583
minabe <minabe@neopa.jp>
2021-02-12 15:56:36 +0900

    axios post

--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,21 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
+      <v-card class="bg-color-white">
+        <h3>掲示板に投稿する</h3>
+        <label for="name">
+          ニックネーム
+        </label>
+        <input id="name" type="text" v-model="name">
+        <br><br>
+        <label for="comment">
+          コメント
+        </label>
+        <textarea id="comment" v-model="comment"></textarea>
+        <br><br>
+        <v-btn @click="createComment">送信</v-btn>
+        {{ sendResult }}
+        <h2>掲示板</h2>
       </v-card>
     </v-col>
   </v-row>
@@ -79,11 +24,50 @@
 <script>
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import axios from 'axios'
 export default {
   components: {
     Logo,
     VuetifyLogo
+  },
+  data(){
+    return{
+      name:'',
+      comment:'',
+      sendResult:''
+    }
+  },
+  methods:{
+    createComment(){
+      axios.post('https://firestore.googleapis.com/v1/projects/seiya-project/databases/(default)/documents/comments',{
+          fields: {
+            name:{
+            stringValue:this.name
+            },
+            comment:{
+              stringValue: this.comment
+            }
+          }
+        }
+        )
+      .then(response => {
+        console.log(response);
+        if(response.status === 200){
+          this.sendResult = '成功'
+        }
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+      this.name=''
+      this.comment=''
+    }
   }
 }
 </script>
+<style scoped lang="scss">
+.bg-color-white{
+  background-color: white;
+  color: black;
+}
+</style>
